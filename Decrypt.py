fb2f4189d2d493c465a2f5dc8686c9fb2c60f794
Koushik <76649479+vkoushik7@users.noreply.github.com>
2023-06-15 10:12:36 +0530

    Delete Decrypt.py

--- Decrypt.py
+++
@@ -1,8 +0,0 @@
-import base64
-from itertools import cycle
-
-message = """DUwBHBALCxIYSFVJSE4MRRMKBk5fSEkCBAMZFgkOHlJRS0hJVA0dFQ4KGBYMTkcXUQ4UDxwaGhJM T09TTwAFVAQOFgARBAtGR09SEgsBAlIADh8MHRxJQVFPUgYGBQRUHQ4WTl9ISRMKDRcaHBpMF0xL VRoSDgtGR09SFQcGTBdMS1UeGgZPRhY="""
-
-key = bytes("vkrishnakoushik7", "utf8")
-
-print(bytes(a ^ b for a, b in zip(base64.b64decode(message), cycle(key))))
